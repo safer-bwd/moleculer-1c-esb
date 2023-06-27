a1d2bb9489c09e810f40c9ed311c4e77c7bacfd0
Anton Safronov <safer_bwd@mail.ru>
2023-06-27 16:20:59 +0300

    refactor: create application worker

---
+++ src/application.js
@@ -0,0 +1,140 @@
+const { Connection, ConnectionEvents } = require('1c-esb');
+const { merge, get, pick } = require('./utils');
+
+const ChannelDirections = {
+  In: 'in',
+  Out: 'out'
+};
+
+class Application {
+  constructor(service, options) {
+    this._options = options;
+
+    const { id } = options;
+    this._id = id;
+    this._connection = null;
+    this._senders = new Map();
+    this._recievers = new Map();
+
+    this._service = service;
+    this._logger = this._createLogger();
+  }
+
+  async connect() {
+    this._logger.debug('connecting...');
+    this._connection = await this._openConnection();
+    // TODO: reconnect on close
+    await this._createLinks();
+    this._logger.info('connected.');
+  }
+
+  async close() {
+    this._logger.debug('closing...');
+    await this._closeLinks();
+    await this._closeConnection();
+    this._logger.info('closed.');
+  }
+
+  async _openConnection() {
+    const connOpts = merge(pick(this._options, [
+      'url', 'clientKey', 'clientSecret', 'reconnect', 'defaultOperationTimeoutInSeconds'
+    ]), {
+      amqp: get(this._options, 'amqp.connection', {}),
+    });
+
+    const connection = new Connection(connOpts);
+    connection
+      .on(ConnectionEvents.connectionOpen, () => {
+        this._logger.info('connection opened.');
+      })
+      .on(ConnectionEvents.connectionError, (ctx) => {
+        this._logger.debug('connection error.', ctx.error);
+      })
+      .on(ConnectionEvents.error, (ctx) => {
+        this._logger.debug('error.', ctx.error);
+      })
+      .on(ConnectionEvents.protocolError, (ctx) => {
+        this._logger.debug('protocol error.', ctx.error);
+      })
+      .on(ConnectionEvents.disconnected, (ctx) => {
+        this._logger.info('disconnected.', ctx.error);
+      })
+      .on(ConnectionEvents.connectionClose, () => {
+        this._logger.info('connection closed.');
+      });
+
+    this._logger.debug('connection opening...');
+
+    try {
+      await connection.open();
+    } catch (err) {
+      this._logger.error('failed to open connection.', err);
+      throw err;
+    }
+
+    return connection;
+  }
+
+  async _closeConnection() {
+    this._logger.debug('connection closing...');
+    await this._connection.close();
+  }
+
+  async _createLinks() {
+    this._logger.debug('links creating...');
+
+    const { channels } = this._options;
+    const channelNames = Object.keys(channels);
+
+    await Promise.all(channelNames.map(async (name) => {
+      if (channels[name].direction === ChannelDirections.In) {
+        // TODO: reciever
+      } else {
+        const sender = await this._createSender(name);
+        this._senders.set(name, sender);
+      }
+    }));
+
+    this._logger.info('links created.');
+  }
+
+  async _closeLinks() {
+    this._logger.debug('links closing...');
+
+    const links = []
+      .concat(Array.from(this._senders.values()))
+      .concat(Array.from(this._recievers.values()));
+
+    await Promise.all(links.map((link) => link.close()));
+
+    this._logger.info('links closed.');
+  }
+
+  async _createSender(channelName) {
+    this._logger.debug(`sender for '${channelName}' creating...`);
+
+    const { channels } = this._options;
+    const [process, channel] = channelName.split('.');
+
+    const senderOpts = merge({}, get(this._options, 'amqp.sender', {}), channels[channelName].amqp);
+    const sender = await this._connection.createAwaitableSender(process, channel, senderOpts);
+
+    this._logger.info(`sender for '${channelName}' created.`);
+
+    return sender;
+  }
+
+  _createLogger() {
+    const prefix = `1C:ESB application '${this._id}':`;
+
+    const methods = ['info', 'warn', 'error', 'debug'];
+    const logger = methods.reduce((acc, method) => {
+      acc[method] = this._service.logger[method].bind(this._service.logger, prefix);
+      return acc;
+    }, {});
+
+    return logger;
+  }
+}
+
+module.exports = Application;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-const { Connection, ConnectionEvents } = require('1c-esb');
-const { merge, get, pick } = require('./utils');
-
-const ChannelDirections = {
-  In: 'in',
-  Out: 'out'
-};
+const { merge } = require('./utils');
+const Application = require('./application');
 
 const defaultOperationTimeoutInSeconds = 60;
 
 module.exports = {
   settings: {
     esb: {
-      operationTimeoutInSeconds: 60,
-
       reconnect: {
         initialDelay: 100,
         maxDelay: 60 * 1000,
         limit: Infinity,
       },
+
+      operationTimeoutInSeconds: defaultOperationTimeoutInSeconds,
 
       // https://github.com/amqp/rhea
       amqp: {
@@ -41,114 +36,50 @@ module.exports = {
   applications: {},
 
   methods: {
-    async _connectApp(id) {
-      const eventPrefix = `1C:ESB application '${id}'`;
-      const options = merge(this.settings.esb, this.schema.applications[id]);
-      const senders = new Map();
-      const recivers = new Map();
-
-      const app =  { 
-        id, connection: null, senders, recivers, options, eventPrefix
-      };
-
-      await this._openAppConnection(app);
-      await this._attachAppChannels(app);
-
-      return app;
-    },
-
-    async _openAppConnection(app) {
-      const { options, eventPrefix } = app;
-
-      const connOpts = merge(pick(options, [
-        'url', 'clientKey', 'clientSecret', 'reconnect', 'defaultOperationTimeoutInSeconds'
-      ]), {
-        amqp: get(options, 'amqp.connection', {}),
-      });
-
-      const connection = new Connection(connOpts);
-      connection
-        .on(ConnectionEvents.connectionOpen, () => {
-          this.logger.info(`${eventPrefix}: connection opened.`);
-        })
-        .on(ConnectionEvents.connectionError, (ctx) => {
-          this.logger.warn(`${eventPrefix}: connection error.`, ctx.error);
-        })
-        .on(ConnectionEvents.error, (ctx) => {
-          this.logger.debug(`${eventPrefix}: error.`, ctx.error);
-        })
-        .on(ConnectionEvents.protocolError, (ctx) => {
-          this.logger.debug(`${eventPrefix}: protocol error.`, ctx.error);
-        })
-        .on(ConnectionEvents.disconnected, (ctx) => {
-          this.logger.warn(`${eventPrefix}: disconnected.`, ctx.error);
-        })
-        .on(ConnectionEvents.connectionClose, () => {
-          this.logger.info(`${eventPrefix}: connection closed.`);
-        });
-
-      this.logger.debug(`${eventPrefix}: connecting...`);
-
-      try {
-        await connection.open();
-      } catch (err) {
-        this.logger.error(`${eventPrefix}: fail to open connection.`, err);
-        throw err;
-      }
-
-      app.connection = connection;
-    },
-
-    async _attachAppChannels(app) {
-      const { connection, options, eventPrefix } = app;
-      const { channels } = options;
-
-      this.logger.debug(`${eventPrefix}: attaching channels...`);
-
-      const promises = channels.map(async(ch) => {    
-        const key = `${ch.process}.${ch.channel}`;
-        if (ch.direction === ChannelDirections.In) {
-          this.logger.debug(`${eventPrefix}: attaching reciever for '${key}'...`);
-
-
-          this.logger.info(`${eventPrefix}: attached reciever for '${key}'.`);
-        } else {
-          this.logger.debug(`${eventPrefix}: attaching sender for '${key}'...`);
-          const senderOpts = merge(get(options, 'amqp.sender', {}), ch.options); 
-          const sender = await connection.createAwaitableSender(ch.process, ch.channel, senderOpts);
-          app.senders.set(key, sender);
-          this.logger.info(`${eventPrefix}: attached sender for '${key}'.`);
-        }
-      });
-
-      this.logger.debug(`${eventPrefix}: all channels attached.`);
-
-      await Promise.all(promises);
-    }
   },
 
   created() {
     this._apps = new Map();
-  },
 
-  async started() {
-    this._apps = new Map();
-
-    const appIds = Object.keys(this.schema.applications);
-    if (appIds.length === 0) {
+    const appIDs = Object.keys(this.schema.applications);
+    if (appIDs.length === 0) {
       return;
     }
 
-    this.logger.info('♻ Connect 1C:ESB applications...');
+    this.logger.debug('1C:ESB applications creating...');
 
-    const promises = appIds.map(async(id) => {
-      const app = await this._connectApp(id);
+    appIDs.forEach((id) => {
+      const options = merge({}, this.settings.esb, this.schema.applications[id], { id });
+      const app = new Application(this, options);
       this._apps.set(id, app);
     });
 
-    await Promise.all(promises);
+    this.logger.debug('1C:ESB applications created.');
+  },
+
+  async started() {
+    if (this._apps.size === 0) {
+      return;
+    }
+
+    this.logger.debug('1C:ESB applications connecting...');
+
+    const apps = Array.from(this._apps.values());
+    await Promise.all(apps.map((app) => app.connect()));
+
+    this.logger.debug('1C:ESB applications connected.');
   },
 
   async stopped() {
+    if (this._apps.size === 0) {
+      return;
+    }
+
+    this.logger.debug('1C:ESB applications closing...');
+
+    const apps = Array.from(this._apps.values());
+    await Promise.all(apps.map((app) => app.close()));
+
+    this.logger.debug('1C:ESB applications closed.');
   },
 };
